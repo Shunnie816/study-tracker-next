eaccb30ed9cc580e6ecba0ca41595c2091fd38dd
Shun Yoshiya <ponyo.shun0816@gmail.com>
2024-01-04 10:22:55 +0900

    Menuアイコンを追加

--- src/components/Atoms/BaseIcon/index.tsx
+++ src/components/Atoms/BaseIcon/index.tsx
@@ -2,8 +2,9 @@ import React, { FC } from "react";
 import { IconProps } from "@mui/material/Icon";
 import EditIcon from "@mui/icons-material/Edit";
 import DeleteIcon from "@mui/icons-material/Delete";
+import MenuIcon from "@mui/icons-material/Menu";
 
-type Icon = "edit" | "delete";
+type Icon = "edit" | "delete" | "menu";
 type BaseProps = {
   icon: Icon;
 };
@@ -12,6 +13,7 @@ type Props = BaseProps & Pick<IconProps, "color" | "fontSize">;
 const iconMap = {
   edit: EditIcon,
   delete: DeleteIcon,
+  menu: MenuIcon,
 };
 
 export const BaseIcon: FC<Props> = ({ icon, color, fontSize }) => {
