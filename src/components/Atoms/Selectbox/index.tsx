a95a36be5bd26244e66514ed2290b04e4b8d96e2
Shun Yoshiya <ponyo.shun0816@gmail.com>
2024-02-01 08:55:35 +0900

    validationのだしわけができるように

--- src/components/Atoms/Selectbox/index.tsx
+++ src/components/Atoms/Selectbox/index.tsx
@@ -1,5 +1,4 @@
-import React, { FC } from "react";
-import Box from "@mui/material/Box";
+import React, { forwardRef } from "react";
 import InputLabel from "@mui/material/InputLabel";
 import MenuItem from "@mui/material/MenuItem";
 import FormControl from "@mui/material/FormControl";
@@ -7,48 +6,61 @@ import Select, { SelectChangeEvent } from "@mui/material/Select";
 import FormHelperText from "@mui/material/FormHelperText";
 
 type BaseProps = {
-  id?: string;
-  value: string;
   label: string;
-  /** MenuItemに格納する値をまとめた配列*/
-  items: Array<string>;
+  value: string;
+  /** MenuItemに格納する値をまとめた配列、validateを使わないときのみ定義 */
+  options?: Array<string>;
   /**MenuItemに'分'などを付け足したいときに使用 */
   menuItemText?: string;
-  error?: boolean;
-  errorMessage: string;
   onChange: (event: SelectChangeEvent<string>, child: React.ReactNode) => void;
+  validate?: {
+    error: boolean;
+    errorMessage: string;
+  };
+  children?: React.ReactNode;
 };
 
 type SelectProps = React.ComponentPropsWithoutRef<"select">;
 type Props = BaseProps & Omit<SelectProps, keyof BaseProps>;
 
-export const Selectbox: FC<Props> = ({
-  id,
-  value,
-  label,
-  items,
-  menuItemText,
-  error,
-  errorMessage,
-  onChange,
-}) => {
-  return (
-    <FormControl fullWidth error={error}>
-      <InputLabel id={label}>{label}</InputLabel>
-      <Select
-        labelId={label}
-        id={id}
-        value={value}
-        label={label}
-        onChange={onChange}
-      >
-        {items.map((item, index) => (
-          <MenuItem value={item} key={index}>
-            {menuItemText ? item + menuItemText : item}
-          </MenuItem>
-        ))}
-      </Select>
-      {error && <FormHelperText>{errorMessage}</FormHelperText>}
-    </FormControl>
-  );
-};
+// eslint-disable-next-line react/display-name
+export const Selectbox = forwardRef(
+  (
+    {
+      id,
+      label,
+      value,
+      options,
+      menuItemText,
+      onChange,
+      validate,
+      children,
+    }: Props,
+    ref
+  ) => {
+    return (
+      <FormControl fullWidth error={validate?.error}>
+        <InputLabel id={label}>{label}</InputLabel>
+        <Select
+          labelId={label}
+          id={id}
+          label={label}
+          value={value}
+          onChange={onChange}
+          ref={ref}
+        >
+          {children
+            ? children
+            : options?.map((item, index) => (
+                <MenuItem value={item} key={index}>
+                  {menuItemText ? item + menuItemText : item}
+                </MenuItem>
+              ))}
+        </Select>
+        {validate?.error && (
+          <FormHelperText>{validate?.errorMessage}</FormHelperText>
+        )}
+      </FormControl>
+    );
+  }
+);
