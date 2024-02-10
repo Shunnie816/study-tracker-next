9973af9a25bec9cc6cf524f247065ff3dd1810ba
Shun Yoshiya <ponyo.shun0816@gmail.com>
2024-02-10 15:13:27 +0900

    apply global backgournd color

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "../assets/styles/reset.scss";
+import "../assets/styles/global.scss";
 import "../assets/styles/variable.scss";
 import { Layout } from "@/components/Templates/Layout";
 
