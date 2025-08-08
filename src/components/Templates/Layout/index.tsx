fd7b2637c90c7c5544812410c9d59fd160afea37
Shunnie816 <ponyo.shun0816@gmail.com>
2025-08-08 09:13:33 +0900

    add header-height variable

--- src/components/Templates/Layout/index.tsx
+++ src/components/Templates/Layout/index.tsx
@@ -1,5 +1,5 @@
 import React, { FC } from "react";
-import { Header } from "../../Organisms/Header";
+import { Header } from "../Header";
 import styles from "./index.module.scss";
 
 type Props = {
