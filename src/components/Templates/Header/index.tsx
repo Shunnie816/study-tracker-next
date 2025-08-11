d543a8b5b4a1fa4b90dfcdcea71a245ce5a52fe6
Shunnie816 <ponyo.shun0816@gmail.com>
2025-08-11 10:03:57 +0900

    fix: apply responsive design for header

--- src/components/Templates/Header/index.tsx
+++ src/components/Templates/Header/index.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { FC, useEffect, useState } from "react";
+import React, { FC, useState } from "react";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
@@ -9,24 +9,14 @@ import { HamburgerMenu } from "@/components/Molecules/HamburgerMenu";
 import styles from "./index.module.scss";
 import { Button } from "@/components/Atoms/Button";
 import Link from "next/link";
+import { clns } from "@/libs/utils";
+import { useDeviceSize } from "@/libs/hooks/useDeviceSize";
 
 type Props = {};
 
 export const Header: FC<Props> = () => {
-  const [state, setState] = useState<boolean>(false);
-  const [isMobile, setIsMobile] = useState<boolean>(false);
-
-  useEffect(() => {
-    const handleResize = () => {
-      setIsMobile(window.innerWidth < 768);
-    };
-    window.addEventListener("resize", handleResize);
-
-    // コンポーネントがアンマウントされた時にイベントリスナーを削除
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, []);
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const { isPC } = useDeviceSize();
 
   return (
     <Box sx={{ flexGrow: 1, width: "100%", position: "fixed", zIndex: "100" }}>
@@ -35,42 +25,40 @@ export const Header: FC<Props> = () => {
           <Typography color="inherit" variant="h6" component="a" href="/">
             Study Tracker
           </Typography>
-          {isMobile ? (
-            <div className={styles.mobileHeader}>
-              <a onClick={() => setState(true)}>
-                <Icon icon={"menu"} color="inherit" />
-              </a>
-              <HamburgerMenu state={state} setState={setState} />
-            </div>
-          ) : (
-            <div className={styles.pcHeader}>
-              <Button color="inherit" variant="text">
-                <Link href="/" className={styles.link}>
-                  ログイン
-                </Link>
-              </Button>
-              <Button color="inherit" variant="text">
-                <Link href="/" className={styles.link}>
-                  記録する
-                </Link>
-              </Button>
-              <Button color="inherit" variant="text">
-                <Link href="/register" className={styles.link}>
-                  教材登録
-                </Link>
-              </Button>
-              <Button color="inherit" variant="text">
-                <Link href="/posts" className={styles.link}>
-                  学習記録
-                </Link>
-              </Button>
-              <Button color="inherit" variant="text">
-                <Link href="/study-log" className={styles.link}>
-                  学習時間
-                </Link>
-              </Button>
-            </div>
-          )}
+          <div className={clns(styles.pcHeader, isPC && styles.isPC)}>
+            <Button color="inherit" variant="text">
+              <Link href="/" className={styles.link}>
+                ログイン
+              </Link>
+            </Button>
+            <Button color="inherit" variant="text">
+              <Link href="/" className={styles.link}>
+                記録する
+              </Link>
+            </Button>
+            <Button color="inherit" variant="text">
+              <Link href="/register" className={styles.link}>
+                教材登録
+              </Link>
+            </Button>
+            <Button color="inherit" variant="text">
+              <Link href="/posts" className={styles.link}>
+                学習記録
+              </Link>
+            </Button>
+            <Button color="inherit" variant="text">
+              <Link href="/study-log" className={styles.link}>
+                学習時間
+              </Link>
+            </Button>
+          </div>
+          {/* ハンバーガーメニュー */}
+          <div className={clns(styles.mobileHeader, isPC && styles.isPC)}>
+            <a onClick={() => setIsOpen(true)}>
+              <Icon icon={"menu"} color="inherit" />
+            </a>
+            <HamburgerMenu state={isOpen} setState={setIsOpen} />
+          </div>
         </Toolbar>
       </AppBar>
     </Box>
