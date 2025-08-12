8b83539ed9489ed535ac92fe82834b9b65b01be5
Shunnie816 <ponyo.shun0816@gmail.com>
2025-08-12 09:39:33 +0900

    set basePath

--- next.config.js
+++ next.config.js
@@ -2,6 +2,8 @@ const path = require("path");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // shunniehub.comのサブドメインにするため、ルートを/study-trackerに設定
+  basePath: "/study-tracker",
   sassOptions: {
     additionalData: `@import "@/assets/styles/breakpoint.scss";`,
     includePaths: [path.join(__dirname, "src")],
