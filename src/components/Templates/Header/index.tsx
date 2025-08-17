5204d106db1d63d70990de75e4ce17584c44900d
Shunnie816 <ponyo.shun0816@gmail.com>
2025-08-17 17:18:11 +0900

    modify Header and HamburgerMenu

--- src/components/Templates/Header/index.tsx
+++ src/components/Templates/Header/index.tsx
@@ -3,11 +3,11 @@ import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
-import Link from "next/link";
 import React from "react";
-import { Button } from "@/components/Atoms/Button";
 import { Icon } from "@/components/Atoms/Icon";
+import { ListMenu } from "@/components/Atoms/ListMenu";
 import { HamburgerMenu } from "@/components/Organisms/HamburgerMenu";
+import { URL_VALUES } from "@/libs/constants/url";
 import { useDeviceSize } from "@/libs/hooks/useDeviceSize";
 import { clns } from "@/libs/utils/clns/indext";
 import styles from "./index.module.scss";
@@ -15,42 +15,22 @@ import { useHamburgerMenu } from "./useHamburgerMenu";
 
 export function Header() {
   const { isPC } = useDeviceSize();
-  const { isOpen, setIsOpen, toggleDrawer, menuItems, icons, urlPath } =
-    useHamburgerMenu();
+  const { isOpen, setIsOpen, toggleDrawer, menuItems } = useHamburgerMenu();
 
   return (
     <Box sx={{ flexGrow: 1, width: "100%", position: "fixed", zIndex: "100" }}>
       <AppBar position="static">
         <Toolbar className={styles.headerMenu}>
-          <Typography color="inherit" variant="h6" component="a" href="/">
+          <Typography
+            color="inherit"
+            variant="h6"
+            component="a"
+            href={URL_VALUES.REPORT}
+          >
             Study Tracker
           </Typography>
           <div className={clns(styles.pcHeader, isPC && styles.isPC)}>
-            <Button color="inherit" variant="text">
-              <Link href="/" className={styles.link}>
-                ログイン
-              </Link>
-            </Button>
-            <Button color="inherit" variant="text">
-              <Link href="/" className={styles.link}>
-                記録する
-              </Link>
-            </Button>
-            <Button color="inherit" variant="text">
-              <Link href="/register" className={styles.link}>
-                教材登録
-              </Link>
-            </Button>
-            <Button color="inherit" variant="text">
-              <Link href="/posts" className={styles.link}>
-                学習記録
-              </Link>
-            </Button>
-            <Button color="inherit" variant="text">
-              <Link href="/study-log" className={styles.link}>
-                学習時間
-              </Link>
-            </Button>
+            <ListMenu items={menuItems} row />
           </div>
           {/* ハンバーガーメニュー */}
           <div className={clns(styles.mobileHeader, isPC && styles.isPC)}>
@@ -61,8 +41,6 @@ export function Header() {
               isOpen={isOpen}
               setIsOpen={setIsOpen}
               menuItems={menuItems}
-              icons={icons}
-              urlPath={urlPath}
               toggleDrawer={toggleDrawer}
             />
           </div>
---
+++ src/libs/constants/url.ts
@@ -0,0 +1,6 @@
+export const URL_VALUES = {
+  REPORT: "/study-tracker",
+  REGISTER: "/register",
+  POSTS: "/posts",
+  STUDYLOG: "/study-log",
+};
