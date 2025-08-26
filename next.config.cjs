06b7835b2aab1cb8afe8e846b6f7df6a6b7c178b
Shunnie816 <ponyo.shun0816@gmail.com>
2025-08-27 08:36:21 +0900

    remove unused setting

--- next.config.cjs
+++ next.config.cjs
@@ -1,13 +1,7 @@
-import path from "path";
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // shunniehub.comのサブドメインにするため、ルートを/study-trackerに設定
   basePath: "/study-tracker",
-  sassOptions: {
-    additionalData: `@import "@/assets/styles/breakpoint.scss";`,
-    includePaths: [path.join(process.cwd(), "src")],
-  },
 };
 
 export default nextConfig;
